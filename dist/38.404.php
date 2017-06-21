d0960707ae365629ce2b8f3d281c07263befc579
Deffhyper <axelogykk@mail.ru>
2017-06-21 11:55:12 +0300

    404 - 100%

---
+++ dist/38.404.php
@@ -0,0 +1,33 @@
+<?php include_once "parts/head.php" ?>
+<body>
+<?php include_once "parts/header.php" ?>
+
+<?php include_once "parts/breadcrumbs.php" ?>
+
+<div class="container">
+    <div class="row">
+        <h1 class="page-title">Ошибка 404. Страница не найдена</h1>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="error-block margin-bottom-lg">
+            <div class="error-block__back" style="background: url('img/jpeg_files/404-image.jpg') no-repeat center center / contain;">
+                <span class="text-bold">404</span>
+                <span class="text-accent">error</span>
+            </div>
+            <div class="error-block__bottom">
+                <p>Страница, на которую вы пытаетесь зайти не найдена. Возможно, была допущена ошибка в написании адреса.</p>
+                <p>Попробуйте посетить другой раздел сайта.</p>
+                <a href="/" class="btn btn-round btn--blue">На главную</a>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+<?php include_once "parts/footer.php" ?>
+</body>
+</html>
